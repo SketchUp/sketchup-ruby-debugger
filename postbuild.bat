390099ab4e4f5d8672974507d5db66f7595d3018
unknown <bbarin@bbarin-westd1.am.trimblecorp.net>
2014-01-14 15:11:04 -0700

    Added postbuild.bat

---
+++ postbuild.bat
@@ -0,0 +1,4 @@
+rem Comment these out to post-build copy the DLL to SU installation directory.
+rem set SUDIR=C:\Program Files (x86)\SketchUp\SketchUp 2014
+rem xcopy /dy %TARGETDIR%SURubyDebugger.dll %SUDIR%
+rem xcopy /dy %TARGETDIR%SURubyDebugger.pdb %SUDIR%
